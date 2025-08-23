4c70538504e56bd0696ca2f9454c06670e63aaf7
Alberto_Desastre <armassantanaalberto@gmail.com>
2025-08-23 21:00:04 +0100

    separated stack display to its own component

---
+++ src/components/StackDisplay.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- Stack tecnológico -->
+  <h3>Stack tecnológico</h3>
+
+  <CardCaroussel :imgs />
+</template>
+
+<script setup lang="ts">
+/* componentes */
+import type { CardImage } from './CardCaroussel.vue'
+import CardCaroussel from './CardCaroussel.vue'
+/* imagenes */
+import jslogo from '@/imgs/js.png'
+import nest from '@/imgs/nest.js.png'
+import css3 from '@/imgs/css3.png'
+import git from '@/imgs/git.png'
+import github from '@/imgs/github.png'
+import html5 from '@/imgs/html5.png'
+import jest from '@/imgs/jest.png'
+import nodejs from '@/imgs/node.js.png'
+import react from '@/imgs/react.png'
+import sass from '@/imgs/sass.png'
+import swagger from '@/imgs/swagger.png'
+import tailwind from '@/imgs/tailwind-css.png'
+import typescript from '@/imgs/typescript.png'
+import vscode from '@/imgs/visual-studio-code.png'
+import vue from '@/imgs/vue.js.png'
+
+const imgs: CardImage[] = [
+  { src: jslogo, text: 'Javascript' },
+  { src: typescript, text: 'TypeScript' },
+  { src: react, text: 'React' },
+  { src: vue, text: 'Vue.js' },
+  { src: html5, text: 'HTML5' },
+  { src: css3, text: 'CSS3' },
+  { src: tailwind, text: 'Tailwind' },
+  { src: sass, text: 'Sass' },
+  { src: jest, text: 'Jest' },
+  { src: swagger, text: 'Swagger' },
+  { src: git, text: 'Git' },
+  { src: github, text: 'GitHub' },
+  { src: nodejs, text: 'Node.js' },
+  { src: nest, text: 'Nest.js' },
+  { src: vscode, text: 'VS Code' },
+]
+</script>
